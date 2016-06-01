a46efd847d031060df232da8e5b293ec06667900
Jani Makinen <coding@live.com>
2016-06-01 20:26:44 +0300

    Created xorcrypting.py for exercising Python basics through cryptology
    Trying our some python in the form of some basic cryptology exercises

---
+++ cryptology/XOR/xorcrypting.py
@@ -0,0 +1,7 @@
+#The purpose of this file is to exercise some Python via basic cryptology
+
+def xor_encrypt(text, key):
+    print("xor_encrypt: ", text, key)
+
+def xor_decrypt(text, key):
+    print("xor_decrypt: ", text, key)
